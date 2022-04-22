9ac67966fe22915fa728f4c2a5d3468c3b6f239b
leminhkhanh81 <khanhle.t3@gmail.com>
2022-04-22 09:16:00 +0700

    Initial Commit

---
+++ RGBullsEyeSwiftUI/RGBullsEyeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBullsEyeSwiftUIApp.swift
+//  RGBullsEyeSwiftUI
+//
+//  Created by Le Minh Khanh on 22/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct RGBullsEyeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
